37f92acfe9380b51605fd35657497da048b1874e
porplatakkom <apiwat.sri@icloud.com>
2021-02-11 22:33:10 +0700

    [UPDATE] edit edit design and add image

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const app = {
     data() {
         return {
-            tasks: [{src:'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png', detail: 'Bulbasaur (Japanese: フシギダネ Fushigidane) is a dual-type Grass/Poison Pokémon introduced in Generation I.', show: false, like: false},
-            {src:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png', detail: 'Charmander (Japanese: ヒトカゲ Hitokage) is a Fire-type Pokémon introduced in Generation I.', show: false, like: false},
-            {src:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png', detail: 'Squirtle (Japanese: ゼニガメ Zenigame) is a Water-type Pokémon introduced in Generation I.', show: false, like: false}
+            tasks: [
+
+            {src:'./images/001.png', 
+            detail: 'Bulbasaur (Japanese: フシギダネ Fushigidane) is a dual-type Grass/Poison Pokémon introduced in Generation I.', 
+            show: false, 
+            like: false},
+
+            {src:'./images/004.png', 
+            detail: 'Charmander (Japanese: ヒトカゲ Hitokage) is a Fire-type Pokémon introduced in Generation I.', 
+            show: false, 
+            like: false},
+
+            {src:'./images/007.png', 
+            detail: 'Squirtle (Japanese: ゼニガメ Zenigame) is a Water-type Pokémon introduced in Generation I.', 
+            show: false, 
+            like: false}
                     ]
+
         }
     },
     methods: {
@@ -13,6 +27,11 @@ const app = {
         },
         toggleLike(index){
             this.tasks[index].like = !this.tasks[index].like
+        }
+    },
+    computed: {
+        countLike(){
+            return this.tasks.filter( t => t.like ).length
         }
     }
 }
