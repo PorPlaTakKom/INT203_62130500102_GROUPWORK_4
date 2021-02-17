e161fa93a61712fa84cd642975493f301440e5f8
porplatakkom <apiwat.sri@icloud.com>
2021-02-18 03:11:40 +0700

    [UPDATE] add search bar

--- main.js
+++ main.js
@@ -4,34 +4,48 @@ const app = {
             tasks: [
 
             {src:'./images/001.png', 
-            detail: 'Bulbasaur (Japanese: フシギダネ Fushigidane) is a dual-type Grass/Poison Pokémon introduced in Generation I.', 
+            detail: 'Bulbasaur (Japanese: フシギダネ Fushigidane) is a dual-type Grass/Poison Pokémon introduced in Generation I.',
+            name: 'Bulbasaur', 
             show: false, 
             like: false},
 
             {src:'./images/004.png', 
             detail: 'Charmander (Japanese: ヒトカゲ Hitokage) is a Fire-type Pokémon introduced in Generation I.', 
+            name: 'Charmander', 
             show: false, 
             like: false},
 
             {src:'./images/007.png', 
             detail: 'Squirtle (Japanese: ゼニガメ Zenigame) is a Water-type Pokémon introduced in Generation I.', 
+            name: 'Squirtle', 
             show: false, 
             like: false}
-                    ]
+                    ],
+            searchText : '',
+            search: false
 
         }
     },
     methods: {
         toggleShow(index){
-            this.tasks[index].show = !this.tasks[index].show
+            this.filterText[index].show = !this.filterText[index].show
         },
         toggleLike(index){
-            this.tasks[index].like = !this.tasks[index].like
+            // this.tasks[index].like = !this.tasks[index].like
+            this.filterText[index].like = !this.filterText[index].like
+        },
+        toggleSearch(){
+            this.search = !this.search
         }
     },
     computed: {
         countLike(){
             return this.tasks.filter( t => t.like ).length
+        },
+        filterText(){
+            return this.tasks.filter( d => {
+                return d.name.toLowerCase().includes(this.searchText.toLowerCase())
+            })
         }
     }
 }
